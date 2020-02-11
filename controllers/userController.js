8bedf89b02d19f045f79655e49f56189342ba462
EunsooJung <hi.michael.jung@gmail.com>
2020-02-11 01:17:02 -0800

    Implemented userController to user signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,22 @@
-// Send to router
-exports.sendFromCtl = (req, res) => {
-  res.json({ message: 'Hi form userController!' });
+/**
+ * User Contollers: It's responsibility is a controll business flows for all users
+ */
+
+const User = require('../models/user');
+/**
+ * @method:
+ */
+exports.signup = (req, res) => {
+  console.log('req.body', req.body);
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err
+      });
+      res.json({
+        user
+      });
+    }
+  });
 };
