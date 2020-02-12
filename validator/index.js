2d69ceaf2e04e90efb7f6c200fefd8e0a5e8e40d
EunsooJung <hi.michael.jung@gmail.com>
2020-02-11 21:55:16 -0800

    Modified error.message to error.msg that is express-validator rule

--- validator/index.js
+++ validator/index.js
@@ -23,7 +23,7 @@ exports.userSignupValidater = (req, res, next) => {
 
   const errors = req.validationErrors();
   if (errors) {
-    const firstError = errors.map(error => error.message)[0];
+    const firstError = errors.map(error => error.msg)[0];
     return res.status(400).json({ error: firstError });
   }
   next();
