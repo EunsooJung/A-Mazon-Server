398547eb8612bf2183b918ec614f07aae80ff8ca
EunsooJung <hi.michael.jung@gmail.com>
2020-02-11 21:52:32 -0800

    Applied userSignup Validator

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { signup } = require('../controllers/userController');
+const { userSignupValidator } = require('../validator');
 
 /**
  * @method post signup
+ * @method userSignupValidator from '../validator/index.js
  * @parametors signup
  */
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 module.exports = router;
