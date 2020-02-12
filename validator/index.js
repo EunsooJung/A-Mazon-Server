bf82987b9852ca757872b7f2527275562f5bf079
EunsooJung <hi.michael.jung@gmail.com>
2020-02-11 21:46:44 -0800

    Create helper method to validate user data on signup process

---
+++ validator/index.js
@@ -0,0 +1,30 @@
+/**
+ * Create helper method to validate user data on signup process,
+ * apply to user routes
+ */
+
+exports.userSignupValidater = (req, res, next) => {
+  req.check('name', 'Name is required').notEmpty();
+  req
+    .check('email', 'Email must be between 3 to 32 characters')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+      min: 3,
+      max: 32
+    });
+  req.check('password', 'Password is required').notEmpty();
+  req
+    .check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must contain at least 6 characters')
+    .matches(/\d/)
+    .withMessage('Password must contain a number');
+
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map(error => error.message)[0];
+    return res.status(400).json({ error: firstError });
+  }
+  next();
+};
