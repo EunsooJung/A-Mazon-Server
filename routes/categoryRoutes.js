0637a8dcd340a76088ae6cb1efe64962a29a834d
EunsooJung <hi.michael.jung@gmail.com>
2020-02-13 01:45:37 -0800

    Applied admin auth to create category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { create } = require('../controllers/categoryController');
+const {
+  requireSignin,
+  isAuth,
+  isAdmin
+} = require('../controllers/userAuthController');
+const { userById } = require('../controllers/userController');
 
-router.post('/category/create', create);
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+router.param('userId', userById);
 
 module.exports = router;
