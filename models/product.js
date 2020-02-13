86f9d83a3b247a2c1f5ce728e48b3652bfbcbd14
EunsooJung <hi.michael.jung@gmail.com>
2020-02-13 02:47:52 -0800

    Implemented Product Model

---
+++ models/product.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: true,
+      maxlength: 32
+    },
+    description: {
+      type: String,
+      require: true,
+      maxlength: 2000
+    },
+    price: {
+      type: Number,
+      trim: true,
+      require: true,
+      maxlength: 32
+    },
+    category: {
+      // When we reffered to the product category it will go to category model
+      type: ObjectId,
+      // refer to Category model
+      ref: 'Category',
+      require: true
+    },
+    quantity: {
+      type: Number
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    },
+    shipping: {
+      required: false,
+      type: Boolean
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Product', productSchema);
