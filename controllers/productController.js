f67a36430d30f3572bfc695de097e4b542722ac5
EunsooJung <hi.michael.jung@gmail.com>
2020-02-13 22:22:10 -0800

    Implemented productById meddleware and single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,6 +8,27 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+// Find By productId
+exports.productById = (req, res, next, id) => {
+  Product.findById(id)
+    .populate('category')
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: 'Product not found'
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
+
+// Read
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   // recieve image
   let form = new formidable.IncomingForm();
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/productController');
+const {
+  create,
+  productById,
+  read
+} = require('../controllers/productController');
 const {
   requireSignin,
   isAuth,
@@ -9,8 +13,10 @@ const {
 } = require('../controllers/userAuthController');
 const { userById } = require('../controllers/userController');
 
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
