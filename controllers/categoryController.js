ab8bdddf076f55afd67ecbfc3b84706bf7809345
EunsooJung <hi.michael.jung@gmail.com>
2020-02-14 00:34:53 -0800

    Implemented Find category by Id and single category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,20 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+/** categoryById */
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: 'Category does not exist'
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
+/** Create New Category */
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
@@ -11,4 +25,8 @@ exports.create = (req, res) => {
     }
     res.json({ data });
   });
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/categoryController');
+const {
+  create,
+  categoryById,
+  read
+} = require('../controllers/categoryController');
+
 const {
   requireSignin,
   isAuth,
   isAdmin
 } = require('../controllers/userAuthController');
+
 const { userById } = require('../controllers/userController');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+// middleware
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
