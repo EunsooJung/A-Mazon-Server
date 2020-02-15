d30264f0b9e8dcab6b225a8a46bbdca9dfb56e05
EunsooJung <hi.michael.jung@gmail.com>
2020-02-14 21:45:12 -0800

    Implemented User profile management

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,3 +19,31 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 };
+
+/** Retrieve to User profile */
+exports.retrieveProfile = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+/** Update User Profile */
+exports.updateProfile = (req, res) => {
+  console.log('user update', req.body);
+  req.body.role = 0; // role will always be 0
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'You are not authorized to perform this action'
+        });
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    }
+  );
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,13 +6,22 @@ const {
   isAuth,
   isAdmin
 } = require('../controllers/userAuthController');
-const { userById } = require('../controllers/userController');
+
+const {
+  userById,
+  retrieveProfile,
+  updateProfile
+} = require('../controllers/userController');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
   });
 });
+
+/* User profile */
+router.get('/user/:userId', requireSignin, isAuth, retrieveProfile);
+router.put('/user/:userId', requireSignin, isAuth, updateProfile);
 
 router.param('userId', userById);
 
